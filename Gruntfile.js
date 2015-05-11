74ba1f9eafcd150a6cfe2c7e9abc567c5d221f79
Bernard Labno <bernard.labno@pjwstk.edu.pl>
2015-05-11 06:06:36 +0200

    npm, grunt and livereload

---
+++ Gruntfile.js
@@ -0,0 +1,62 @@
+module.exports = function (grunt)
+{
+
+    // Load grunt tasks automatically
+    require('load-grunt-tasks')(grunt);
+
+
+    var config = {
+        app: './app'
+    };
+
+    grunt.initConfig({
+
+        // Project settings
+        config: config,
+
+        // Watches files for changes and runs tasks based on the changed files
+        watch: {
+            livereload: {
+                options: {
+                    livereload: '<%= connect.options.livereload %>'
+                },
+                files: [
+                    '<%= config.app %>/**/*.js', '<%= config.app %>/**/*.html'
+                ]
+            }
+        },
+        connect: {
+            options: {
+                port: grunt.option('frontend-port') || 9000,
+                hostname: grunt.option('frontend-url') || 'localhost',
+                livereload: 35729
+            },
+            livereload: {
+                options: {
+                    open: true,
+                    middleware: function (connect)
+                    {
+                        return [
+                            connect.static(config.app)
+                        ];
+                    }
+                }
+            },
+            distPreview: {
+                options: {
+                    open: true,
+                    middleware: function (connect)
+                    {
+                        return [
+                            connect.static(config.dist), require('grunt-connect-proxy/lib/utils').proxyRequest
+                        ];
+                    }
+                }
+            }
+        }
+    });
+
+    grunt.registerTask('serve', 'Compile then start a connect web server', [
+        'connect:livereload', 'watch'
+    ]);
+};
