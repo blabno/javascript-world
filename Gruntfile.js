6b08f070d24a62534fc3c99eec7473db8f5f560a
Bernard Labno <bernard.labno@pjwstk.edu.pl>
2015-05-24 21:49:20 +0200

    Fake backend

--- Gruntfile.js
+++ Gruntfile.js
@@ -4,6 +4,74 @@ module.exports = function (grunt)
     // Load grunt tasks automatically
     require('load-grunt-tasks')(grunt);
 
+    function createBackendMock()
+    {
+        var userIdSequence = 3;
+        var users = {
+            1: {
+                id: 1,
+                email: 'bernard.labno@pjwstk.edu.pl',
+                status: 'active'
+            },
+            2: {
+                id: 2,
+                email: 's4237@pjwstk.edu.pl',
+                status: 'inactive'
+            }
+        };
+
+        function handleUserPost(request, response)
+        {
+            var match = request._parsedUrl.path.match('/api/user/(\\d*)');
+            response.writeHead(200, {'Content-Type': 'application/json'});
+            request.on('data', function (data)
+            {
+                var user = JSON.parse(data.toString());
+                if (users[user.id]) {
+                    users[user.id].email = user.email;
+                    users[user.id].status = user.status;
+                } else {
+                    if (!user.id) {
+                        user.id = userIdSequence++;
+                    }
+                    users[user.id] = user;
+                }
+                response.end(JSON.stringify(users[user.id]));
+            });
+        }
+
+        function handlUserGet(request, response)
+        {
+            var match = request._parsedUrl.path.match('/api/user/(\\d+)');
+            if (match) {
+                if (users[match[1]]) {
+                    response.end(JSON.stringify(users[match[1]]));
+                } else {
+                    response.writeHead(404);
+                    response.end();
+                }
+            } else {
+                response.writeHead(200, {'Content-Type': 'application/json'});
+                var userList = [];
+                for (var key in users) {
+                    userList.push(users[key]);
+                }
+                response.end(JSON.stringify(userList));
+            }
+        }
+
+        return function (request, response, next)
+        {
+            if (request._parsedUrl.path.match('/api/user(/\\d+)?')) {
+                if ('POST' === request.method) {
+                    handleUserPost(request, response);
+                } else if ('GET' === request.method) {
+                    handlUserGet(request, response);
+                }
+            }
+            next();
+        }
+    }
 
     var config = {
         app: './app'
@@ -37,7 +105,7 @@ module.exports = function (grunt)
                     middleware: function (connect)
                     {
                         return [
-                            connect.static(config.app)
+                            connect.static(config.app), createBackendMock()
                         ];
                     }
                 }
